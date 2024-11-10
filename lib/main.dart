d12dbcf37a603ed49de7b1f2d44a78d96756740e
Mohammad Taha <mohammad.taha.sartawi@gmail.com>
2024-11-10 11:26:53 +0200

    adding app file and writing MyApp with a sngleton pattern.

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,4 @@
-
 import 'package:flutter/material.dart';
+import 'package:advanced_flutter_arabic/app/app.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
+void main() => runApp(MyApp());
